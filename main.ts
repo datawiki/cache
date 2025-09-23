68cea0f42da56243a36f820e6fb92b2e5e502230
datawiki <andre@datawiki.co.za>
2025-09-23 18:42:48 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+import { handleEntityRequest } from "./routes/entity.ts";
+
+serve(async (req) => {
+  const url = new URL(req.url);
+  if (url.pathname.startsWith("/entity/")) {
+    return await handleEntityRequest(req);
+  }
+  return new Response("Eezy Entity Resolver API", { status: 200 });
+});
 
