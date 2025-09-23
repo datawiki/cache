d16cce9a32d3c1e8edb0a98ad05ec03ccdf50032
datawiki <andre@datawiki.co.za>
2025-09-23 20:45:15 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,1 +1,14 @@
 
+const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
+const SUPABASE_KEY = Deno.env.get("SUPABASE_KEY")!;
+
+export async function getMetadataFromSupabase(id: string) {
+  const res = await fetch(`${SUPABASE_URL}/rest/v1/entity_metadata?id=eq.${id}`, {
+    headers: {
+      apikey: SUPABASE_KEY,
+      Authorization: `Bearer ${SUPABASE_KEY}`,
+    },
+  });
+  const data = await res.json();
+  return data[0];
+}
