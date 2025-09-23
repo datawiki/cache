b4a5086b0621b3a9fa395192c9e96cd18178c832
datawiki <andre@datawiki.co.za>
2025-09-23 20:58:24 +0200

    Update postgres.ts

--- lib/postgres.ts
+++ lib/postgres.ts
@@ -1,17 +1,20 @@
-import { Client } from "https://deno.land/x/postgres/mod.ts";
+import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
 const client = new Client({
-  hostname: Deno.env.get("PGHOST"),
-  user: Deno.env.get("PGUSER"),
-  password: Deno.env.get("PGPASSWORD"),
-  database: Deno.env.get("PGDATABASE"),
+  hostname: Deno.env.get("PGHOST")!,
+  user: Deno.env.get("PGUSER")!,
+  password: Deno.env.get("PGPASSWORD")!,
+  database: Deno.env.get("PGDATABASE")!,
   port: parseInt(Deno.env.get("PGPORT") || "5432"),
 });
 
 await client.connect();
 
 export async function getEntityFromPostgres(id: string) {
-  const result = await client.queryObject(`SELECT * FROM entities WHERE id = $1`, [id]);
+  const result = await client.queryObject(
+    "SELECT * FROM entities WHERE id = $1",
+    [id]
+  );
   return result.rows[0];
 }
 
