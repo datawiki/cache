ef4c00ebe6f3cd2146f2173c473bd8fd558d05db
datawiki <andre@datawiki.co.za>
2025-09-23 20:45:58 +0200

    Update redis.ts

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,1 +1,16 @@
+import { connect } from "https://deno.land/x/redis/mod.ts";
+
+const redis = await connect({
+  hostname: Deno.env.get("REDIS_HOST")!,
+  port: parseInt(Deno.env.get("REDIS_PORT")!),
+  password: Deno.env.get("REDIS_PASSWORD"),
+});
+
+export async function getFromRedis(key: string) {
+  return await redis.get(key);
+}
+
+export async function setInRedis(key: string, value: string) {
+  await redis.set(key, value);
+}
 
